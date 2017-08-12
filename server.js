4b7cd749eb8184060140c8690933b4c1d0f31467
gouravsharma03 <gouravs785@gmail.com>
2017-08-12 20:03:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,10 +6,25 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var arone = {
-    title: 'Article-one',
-   heading:'welcome to article one',
-   content: 'this is a article one'
+var articles = {
+     'arone' : {
+      title: 'Article-one',
+      heading:'welcome to article one',
+       content: 'this is a article one'
+         
+     },
+     'artwo' :{title: 'Article-two',
+      heading:'welcome to article two',
+       content: 'this is a article two'
+         
+     },
+     'arthree': {title: 'Article-three',
+      heading:'welcome to article three',
+       content: 'this is a article three'
+         
+     }
+
+    
 };
 function createtemp(data)
 {
@@ -19,23 +34,23 @@ var content= data.content;
 
 var htmlTemp = 
     ` <html>
-     <head>
-      <title>
-      ${title}
-      </title>
-      <link href="/ui/style.css" rel="stylesheet" />
-     </head>
-    <body><div class="ar">
-     <div>
-        <a href ="/">home</a>
-     </div>
-      <hr>
-        <div>
-        <h4>${heading}</h4>
-        ${content}
+         <head>
+          <title>
+          ${title}
+          </title>
+          <link href="/ui/style.css" rel="stylesheet" />
+         </head>
+        <body><div class="ar">
+         <div>
+            <a href ="/">home</a>
+         </div>
+          <hr>
+            <div>
+            <h4>${heading}</h4>
+            ${content}
+            </div>
         </div>
-    </div>
-    </body>
+        </body>
     </html>
 `;
 return htmlTemp;
@@ -44,16 +59,13 @@ return htmlTemp;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-  res.send(createtemp(arone));
+app.get('/:articleName',function(req,res){
+  res.send(createtemp(articles[articleName]));
                          });
-app.get('/article-two',function(req,res){
-  res.sendFile(path.join(__dirname,'ui','article-two.html'));
-                         });
- app.get('/article-three',function(req,res){
-  res.sendFile(path.join(__dirname,'ui','article-three.html'));
-                         });
+
+
 app.get('/ui/style.css', function (req, res) {
+    var articleName = req.params.articleName;
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
