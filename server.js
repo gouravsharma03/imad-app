82bc3e2436c0fa8503cf4eb3963a73be4fd83b13
gouravsharma03 <gouravs785@gmail.com>
2017-08-12 19:09:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,46 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var arone = {
+    title: 'Article-one',
+   heading:'welcome to article one',
+   content: '<p>this is the first article . and here we will go thorouhg the concepts of luck</p>'
+};
+ function createtemp(data){
+   var title = data.title;
+   var heading = data.heading;
+   var content = data.content;
+    
+ var htmltemp = 
+    <html>
+        <head>
+          <title>${title}</title>
+         <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+     <body>
+       <div class="ar">
+         <div>
+          <a href ="/">home</a>
+         </div>
+         <hr>
+          <div>
+           <h4>${heading}</h4>
+           ${content}
+          </div>
+       </div>
+     </body>
+    </html>
+
+ 
+     return htmltemp;
+};
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname,'ui','article-one.html'));
+  res.send(createtemp((arone));
                          });
 app.get('/article-two',function(req,res){
   res.sendFile(path.join(__dirname,'ui','article-two.html'));
